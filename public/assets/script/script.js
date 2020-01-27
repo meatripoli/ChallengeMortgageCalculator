6444ab6143a3ada878eeb13d17e29b6dd5f40239
Maria Elena Tripoli <meatripoli@gmail.com>
2020-01-27 13:46:36 -0600

    finished the script for input validation and wrote the post request to send data to server for calculation

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -2,12 +2,16 @@ var yearslider = $('#yearofmortgageslider').val();
 var interestslider = $('#interestslider').val();
 $('#currectyear').val(yearslider);
 $('#currectinterest').val(interestslider);
+
+//changes the values of the years and interest box based on the position of the slider
 $('#yearofmortgageslider').change(function() {
     $('#currectyear').val(this.value);
 })
 $('#interestslider').change(function() {
     $('#currectinterest').val(this.value);
 })
+
+//for the color of the slider before position change to blue and after leave as gray
 $('input[type="range"]').change(function () {
     //formula is to change value to add back into the css color-stop
     var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
@@ -16,5 +20,65 @@ $('input[type="range"]').change(function () {
                 + 'color-stop(' + val + ', #1091cc), '
                 + 'color-stop(' + val + ', #d8d8d8)'
                 + ')'
-                );
+    );
 });
+
+//remove color from mandatory field
+$('#loaninput').change(function () {
+    if($('#loaninput').val() === ''){
+        $('#mandatoryfieldloan').css('color', 'red');
+        $('#mandatoryfieldloan').text('Mandatory Field');
+    }
+    else{
+        //$('#mandatoryfieldloan').css('color', '#3e3d3f');
+        $('#mandatoryfieldloan').empty();
+    }
+});
+$('#taxinput').change(function () {
+    if($('#taxinput').val() === ''){
+        $('#mandatoryfieldtax').css('color', 'red');
+        $('#mandatoryfieldtax').text('Mandatory Field');
+    }
+    else{
+        //$('#mandatoryfieldtax').css('color', '#3e3d3f');
+        $('#mandatoryfieldtax').empty();
+    }
+});
+$('#insuranceinput').change(function () {
+    if($('#insuranceinput').val() === ''){
+        $('#mandatoryfieldinsurance').css('color', 'red');
+        $('#mandatoryfieldinsurance').text('Mandatory Field');
+    }
+    else{
+        //$('#mandatoryfieldinsurance').css('color', '#3e3d3f');
+        $('#mandatoryfieldinsurance').empty();
+    }
+});
+
+//on button click send data to server to handle the math
+$('#caluclatebutton').on('click', function (event) {
+    console.log("click calculate button");
+    event.preventDefault();
+    var newObj = {
+        years: $('#currectyear').val(),
+        interestRate: $('#currectinterest').val(),
+        loanAmount: $('#loaninput').val(),
+        annualTax: $('#taxinput').val(),
+        annualInsurance: $('#insuranceinput').val()
+    }
+    console.log('new string captured')
+    console.log(newObj)
+    ///checks if input field is blank or not a number 
+    if(isNaN(parseInt(newObj.loanAmount)) || isNaN(parseInt(newObj.annualTax)) || isNaN(parseInt(newObj.annualInsurance))){
+        console.log('not filled in');
+        ///maybe add an alert
+    }
+    else{
+        console.log('filled in');
+        $('#caluclatebutton').val('RECALCULATE');
+        $.post("/", newObj)
+            .then(function(data) {
+            location.reload();
+        });
+    }
+})
